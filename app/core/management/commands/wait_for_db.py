42efb01e58727a16d82c35064898e22446697995
sibelkahraman <sibelkahraman235@gmail.com>
2020-12-20 01:14:05 +0300

    Fixed linting Please enter the commit message for your changes. Lines starting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         while not db_conn:
             try:
                 db_conn = connections['default']
-            except:
+            except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
