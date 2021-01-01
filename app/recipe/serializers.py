159843c5e4fc76dbaf46240f9334bafaa1768f4c
sibelkahraman <sibelkahraman235@gmail.com>
2021-01-02 00:47:49 +0300

    Added list recipe feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -18,4 +18,19 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe objects"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id',
+                  'title',
+                  'time_minutes',
+                  'price',
+                  'ingredients',
+                  'tags',
+                  'link')
         read_only_fields = ('id',)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 from recipe import serializers
 
@@ -41,3 +41,16 @@ class IngredientViewSet(viewsets.GenericViewSet,
     def perform_create(self, request):
         """Create a new object"""
         request.save(user=self.request.user)
+
+
+class RecipeViewSet(viewsets.GenericViewSet,
+                    mixins.ListModelMixin):
+    """Manage recipes in database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Recipe.objects.all()
+    serializer_class = serializers.RecipeSerializer
+
+    def get_queryset(self):
+        """Return objectsfor current user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
