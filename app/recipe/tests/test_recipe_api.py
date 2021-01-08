30fe9a59b4d14359a4fce4bf9f00c1abfd9bbe81
sibelkahraman <sibelkahraman235@gmail.com>
2021-01-08 23:50:39 +0300

    Added filter feature to recipe api

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -258,3 +258,49 @@ class RecipeImageUploadTests(TestCase):
         res = self.client.post(url, {'image': 'myinage'}, format='multipart')
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_filter_recipes_by_tag(self):
+        """Test filtering recipe with tag"""
+        recipe1 = sample_recipe(user=self.user, title='Vegan Soup')
+        recipe2 = sample_recipe(user=self.user, title='Vegeterian Soup')
+        tag1 = sample_tag(user=self.user, name='Vegan')
+        tag2 = sample_tag(user=self.user, name='Vegeterian')
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag2)
+        recipe3 = sample_recipe(user=self.user, title='Kebab')
+
+        res = self.client.get(
+            RECIPES_URL,
+            {'tags': {f'{tag1.id},{tag2.id}'}}
+            )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
+
+    def test_filter_recipes_by_ingredients(self):
+        """Test filtering recipe with ingredient"""
+        recipe1 = sample_recipe(user=self.user, title='recipe1')
+        recipe2 = sample_recipe(user=self.user, title='recipe2')
+        ingredient1 = sample_ingredient(user=self.user, name='ing1')
+        ingredient2 = sample_ingredient(user=self.user, name='ing2')
+        recipe1.ingredients.add(ingredient1)
+        recipe2.ingredients.add(ingredient2)
+        recipe3 = sample_recipe(user=self.user, title='recipe3')
+
+        res = self.client.get(
+            RECIPES_URL,
+            {'ingredients': {f'{ingredient1.id}, {ingredient2.id}'}}
+        )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -52,9 +52,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def _params_to_ints(self, qs):
+        """Return list of query params ID"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
-        """Return objectsfor current user only"""
-        return self.queryset.filter(user=self.request.user)
+        """Return objects for current user only"""
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tags_ids = self._params_to_ints(tags)
+            print(tags_ids)
+            queryset = queryset.filter(tags__id__in=tags_ids)
+        if ingredients:
+            ingredients_ids = self._params_to_ints(ingredients)
+            print(ingredients_ids)
+            queryset = queryset.filter(ingredients__id__in=ingredients_ids)
+
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
